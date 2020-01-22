7249d029105d1da57d3f3f92bc6d857be913e3d0
Asai Andrade <asaiandrade@gmail.com>
2020-01-22 12:16:33 -0800

    adding correct picture renderings, links and contex

--- src/containers/amazon/amazon.jsx
+++ src/containers/amazon/amazon.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
 
 function Amazon () {
+    const [newData, setNewData] = useState([]);
+    const [picture, setPicture] = useState('');
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        async function getData () {
+            const response = await fetch('http://localhost:3060/amazon');
+            const data = await response.json();
+            setNewData(data.data['results'][0]['locations'][0]);
+            setPicture(data.data['results'][0]['picture']);
+            setName(data.data['results'][0]['name']);
+        }
+
+        getData();
+    }, []);
+
     return (
         <div>
             <Card>
                 <Card.Body>
                     <Card.Title>Amazon</Card.Title>
-                    <Card.Text>Data</Card.Text>
+                    <hr className="w-25 d-flex"/>
+                    { 
+                        newData.length !== 0 ? (
+                            <div>
+                                <h4>{name} - <span><img src={newData.icon} alt='' ></img></span></h4>
+                                <a href={newData.url}>
+                                    <img src={picture} width="300" height="169" alt=''></img>
+                                </a> 
+                            </div>
+                        )
+                        : null 
+                    }
                 </Card.Body>
             </Card>
         </div>
